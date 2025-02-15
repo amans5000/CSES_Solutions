b85e4be797da1f5217d88173340def9358757038
amans5000 <amans5000@gmail.com>
2025-02-15 06:56:39 +0530

    day11

--- DP/CoinCombination2.cpp
+++ DP/CoinCombination2.cpp
@@ -1,53 +1,28 @@
 #include <bits/stdc++.h>
-#define ll long long
-using namespace std;
+#define ll long long int
 #define mod 1000000007
-
-ll coinComb(int n, vector<ll> &dp, vector<int> &coins, int minCoin)
-{
-  if (n == 0)
-  {
-    return 1;
-  }
-
-  if (n < 0)
-  {
-    return 0;
-  }
-
-  if (dp[n] != -1)
-  {
-    return dp[n];
-  }
-
-  ll val = 0;
-  for (int i = minCoin; i < coins.size(); i++)
-  {
-    if (coins[i] <= n)
-      val += coinComb(n - coins[i], dp, coins, i) % mod;
-  }
-
-  dp[n] = val % mod;
-  return dp[n];
-}
+using namespace std;
 
 int main()
 {
-  int n, x;
+
+  ll n, x;
   cin >> n >> x;
-
-  vector<ll> dp(x + 1, -1);
-
-  vector<int> coins(n);
+  vector<ll> coins(n);
   for (int i = 0; i < n; i++)
   {
     cin >> coins[i];
   }
-
   sort(coins.begin(), coins.end());
+  vector<ll> dp(x + 1, 0);
 
-  ll result = coinComb(x, dp, coins, 0);
-  cout << result << endl;
-
-  return 0;
+  dp[0] = 1;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = coins[i]; j <= x; j++)
+    {
+      dp[j] = (dp[j] + dp[j - coins[i]]) % mod;
+    }
+  }
+  cout << dp[x];
 }
