8b26015e496fae1415f607457f77b3a48e79d9db
amans5000 <amans5000@gmail.com>
2025-03-30 10:06:02 +0530

    restart

--- Mathematics/CommonDivisor.cpp
+++ Mathematics/CommonDivisor.cpp
@@ -6,12 +6,32 @@ int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
+
   int n;
   cin >> n;
-  vector<int> arr(n);
+  vector<int> freq(1e6 + 1, 0);
 
   for (int i = 0; i < n; i++)
   {
+    int temp;
+    cin >> temp;
+    if (temp <= 1e6)
+      freq[temp]++;
   }
+
+  for (int i = 1e6; i > 0; i--)
+  {
+    int count = 0;
+    for (int j = i; j <= 1e6; j += i)
+    {
+      count += freq[j];
+      if (count >= 2)
+      {
+        cout << i << endl;
+        return 0;
+      }
+    }
+  }
+
   return 0;
 }
